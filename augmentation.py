00965768501a5311fde1c1bbe59d8eeea4922543
Gal <zr016214@student.reading.ac.uk>
2024-08-12 02:46:15 +0200

    added rotation of csv coordinates for augmentation

--- augmentation.py
+++ augmentation.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+import pandas as pd
+import numpy as np
 import tkinter
 from tkinter import filedialog
 
@@ -40,16 +42,70 @@ def flip_video(directory_path, file_name):
     out.release()
 
 
+def rotate_single_keypoint(x, y, rotation_matrix, center_x, center_y):
+    # translate the point to the center the image, so it rotates about the center instead of the origin (0,0)
+    translated_point = np.array([x - center_x, y - center_y])
+    
+    # apply the rotation matrix
+    rotated_point = rotation_matrix @ translated_point
+    
+    # translate the point back from center
+    new_x, new_y = rotated_point + np.array([center_x, center_y])
+    
+    return new_x, new_y
+
+
+def rotate_keypoints(df, angle, img_width, img_height):
+    # convert angle of rotation to radians
+    theta = np.radians(angle)
+    
+    rotation_matrix = np.array([
+        [np.cos(theta), -np.sin(theta)],
+        [np.sin(theta), np.cos(theta)]
+    ])
+    
+    # find center point of the video frame
+    center_x = img_width / 2
+    center_y = img_height / 2
+    
+    df_rotated = df.copy()
+    
+    for column in df.columns: # for each keypoint extracted from the pose 
+        if '_x' in column:
+            kp_name = column[:-2] 
+            x_col = f'{kp_name}_x'
+            y_col = f'{kp_name}_y'
+
+            
+            # apply the rotation matrix
+            df_rotated[[x_col, y_col]] = df.apply(
+                lambda row: rotate_single_keypoint(row[x_col], row[y_col], rotation_matrix, center_x, center_y), 
+                axis=1, 
+                result_type='expand'
+            )
+    
+    return df_rotated
+
+
+
 
 if __name__ == "__main__":
     tkinter.Tk().withdraw() # prevents an empty tkinter window from appearing
     
-    directory_path = filedialog.askdirectory()
-    directory = os.fsencode(directory_path)
+    # directory_path = filedialog.askdirectory()
+    # directory = os.fsencode(directory_path)
     
-    for file in os.listdir(directory):
-        filename = os.fsdecode(file)
-        if not filename.endswith("mirror.mp4"): # skip any videos already mirrored
-            flip_video(directory_path, filename)
-        else:
-            continue
+    # for file in os.listdir(directory):
+    #     filename = os.fsdecode(file)
+    #     if not filename.endswith("mirror.mp4"): # skip any videos already mirrored
+    #         flip_video(directory_path, filename)
+    #     else:
+    #         continue
+
+    filename = filedialog.askopenfilename()
+
+    df = pd.read_csv(filename, index_col=0)
+
+    df = rotate_keypoints(df, 5, 1080, 1920)
+
+    df.to_csv('test.csv')
